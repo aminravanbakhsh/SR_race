b9c6d706a7b108a84b30ea6276e7565406852381
Ubuntu <asduser@asd.uemk150zhmxexm4y0cgznkchzh.bx.internal.cloudapp.net>
2023-03-12 14:17:35 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SR-race',
+    version='0.01',
+    description='A module to compare symbolic regressor',
+    author='Amin Ravanbakhsh, Joe Scott',
+    author_email='amin.ravanbakhsh@uwaterloo.ca, joseph.scott@uwaterloo.ca',
+    url='https://github.com/aminravanbakhsh/SR_race.git',
+    packages=find_packages()
+)
